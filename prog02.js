5ff7abf8ab94d5c94cd6ad19fc96e10cdb644819
Fermin Ivan Espinosa <ivanx_92@hotmail.com>
2016-02-15 23:38:10 +0000

    commit inicial

---
+++ prog02.js
@@ -0,0 +1,16 @@
+
+n1="";
+function linea(num)
+{
+    
+    for(i=0;i<num;i++)
+    {
+    n1=n1+"-";    
+    }
+    return n1;
+}
+var result
+
+result= linea(3);
+
+console.log(result);
---
+++ prog2.js
@@ -0,0 +1,10 @@
+function suma(n1,n2)
+{
+    return n1+n2;
+}
+
+var result;
+
+result=suma(1,"hola");
+
+console.log(result);
